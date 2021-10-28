9e09f7dab7aeed62361d1bc8f666f37996ed18fb
haknyan <hak.nyan@gmail.com>
2021-10-28 12:44:05 +0900

    migrate

--- django_demo/urls.py
+++ django_demo/urls.py
@@ -18,10 +18,11 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("accounts/", include("accounts.urls")),
-    path("/", include("landing.urls")),
+    path("", include("landing.urls")),
     path("blog/", include("blog.urls")),
     path("shop/", include("shopping.urls")),
     path("payment/", include("payment.urls")),
